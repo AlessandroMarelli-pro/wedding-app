fdc3d07d0dfb61f839671b1378a5657fa1962e45
AlessandroMarelli-pro <alessandro.marelli.pro@gmail.com>
2025-09-09 19:20:31 +0200

    feat: icon display

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,7 +1,7 @@
 import { Button } from '@/components/ui';
 import { Vortex } from '@/components/ui/vortex';
 import { cn } from '@/lib/utils';
-import { IconArrowDown } from '@tabler/icons-react';
+import { IconArrowDown, IconCar, IconTrain } from '@tabler/icons-react';
 import { GetServerSideProps } from 'next';
 import { Parisienne } from 'next/font/google';
 import Head from 'next/head';
@@ -27,6 +27,11 @@ interface HomePageProps {
   weddingInfo: WeddingInfo | null;
   accommodations: any[];
 }
+const WeddingHowToArriveIcons = {
+  car: <IconCar />,
+  train: <IconTrain />,
+  'car rental': <IconCar />,
+};
 
 export default function HomePage({
   weddingInfo,
@@ -226,21 +231,8 @@ export default function HomePage({
                               key={index}
                               className="relative flex flex-col my-6 bg-white shadow-sm border border-slate-200 rounded-lg  p-3"
                             >
-                              <div className="flex items-center mb-1">
-                                <svg
-                                  xmlns="http://www.w3.org/2000/svg"
-                                  fill="none"
-                                  viewBox="0 0 24 24"
-                                  stroke-width="1.5"
-                                  stroke="currentColor"
-                                  className="h-6 w-6 text-slate-600"
-                                >
-                                  <path
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    d="M12 21a9.004 9.004 0 0 0 8.716-6.747M12 21a9.004 9.004 0 0 1-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 0 1 7.843 4.582M12 3a8.997 8.997 0 0 0-7.843 4.582m15.686 0A11.953 11.953 0 0 1 12 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0 1 21 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0 1 12 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 0 1 3 12c0-1.605.42-3.113 1.157-4.418"
-                                  />
-                                </svg>
+                              <div className="flex items-center mb-1 gap-2">
+                                {WeddingHowToArriveIcons[direction.type]}
                                 <h5 className="font-medium text-foreground capitalize">
                                   {direction.type === 'car'
                                     ? 'En voiture'
