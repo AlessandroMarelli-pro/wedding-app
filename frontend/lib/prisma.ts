2d9f6a1d5d151de2505b0bcb7f56094a67c6ec07
AlessandroMarelli-pro <alessandro.marelli.pro@gmail.com>
2025-09-15 22:31:50 +0200

    feat: use neon as recommanded in doc

--- frontend/lib/prisma.ts
+++ frontend/lib/prisma.ts
@@ -1,9 +1,11 @@
+import { PrismaNeon } from '@prisma/adapter-neon';
 import { PrismaClient } from '@prisma/client';
-
 declare global {
   var prisma: PrismaClient | undefined;
 }
-
-export const prisma = global.prisma || new PrismaClient();
+const adapter = new PrismaNeon({
+  connectionString: process.env.POSTGRES_PRISMA_URL,
+});
+export const prisma = global.prisma || new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
