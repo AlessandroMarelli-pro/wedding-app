0915af44bcaa910b2411b732383926c0fa9ec914
AlessandroMarelli-pro <alessandro.marelli.pro@gmail.com>
2025-09-15 23:00:05 +0200

    faet: POSTGRES_URL

--- frontend/lib/prisma.ts
+++ frontend/lib/prisma.ts
@@ -1,11 +1,8 @@
-import { PrismaNeon } from '@prisma/adapter-neon';
 import { PrismaClient } from '@prisma/client';
 declare global {
   var prisma: PrismaClient | undefined;
 }
-const adapter = new PrismaNeon({
-  connectionString: process.env.POSTGRES_PRISMA_URL,
-});
-export const prisma = global.prisma || new PrismaClient({ adapter });
+
+export const prisma = global.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
