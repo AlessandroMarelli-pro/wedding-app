495b7bb57aa77be3a9546e45203ca092d2de14de
AlessandroMarelli-pro <alessandro.marelli.pro@gmail.com>
2025-09-15 20:05:53 +0200

    feat: remove old backend

--- backend/src/entities/index.ts
+++
@@ -1,8 +0,0 @@
-export { Accommodation } from './accommodation.entity';
-export { Admin } from './admin.entity';
-export { CSVUpload, UploadStatus } from './csv-upload.entity';
-export { Guest } from './guest.entity';
-export { ProgramEvent } from './program-event.entity';
-export { RSVPConfirmation } from './rsvp-confirmation.entity';
-export { UploadedImage } from './uploaded-image.entity';
-export { WeddingInfo } from './wedding-info.entity';
