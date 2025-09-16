2ce4b3401153339408ae304ae09ee710c9200cca
AlessandroMarelli-pro <alessandro.marelli.pro@gmail.com>
2025-09-16 12:07:10 +0200

    fix: prisma

--- frontend/lib/prisma.ts
+++ frontend/lib/prisma.ts
@@ -3,5 +3,10 @@ import { withAccelerate } from '@prisma/extension-accelerate';
 declare global {
   var prisma: PrismaClient | undefined;
 }
-
-export const prisma = new PrismaClient().$extends(withAccelerate());
+let prismaClient;
+if (process.env.NODE_ENV === 'production') {
+  prismaClient = new PrismaClient().$extends(withAccelerate());
+} else {
+  prismaClient = new PrismaClient();
+}
+export const prisma = prismaClient;
