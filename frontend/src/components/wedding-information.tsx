6f8b8ff40de204967db70412e8163893ef8407e0
AlessandroMarelli-pro <alessandro.marelli.pro@gmail.com>
2025-09-17 18:50:22 +0200

    feat: justify-around

--- frontend/src/components/wedding-information.tsx
+++ frontend/src/components/wedding-information.tsx
@@ -78,8 +78,8 @@ export const WeddingInformation = ({
               </h1>
             </div>
           </div>
-          <div className="flex flex-row h-[75%] w-full pb-10">
-            <div className="flex flex-col text-center justify-end items-center w-full gap-10">
+          <div className="flex flex-row h-[75%] w-full ">
+            <div className="flex flex-col text-center justify-around items-center w-full gap-10">
               {weddingInfo.locationDirections?.map((direction, index) => (
                 <div
                   key={index}
